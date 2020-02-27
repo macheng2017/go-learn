a55f6d5f8a0e88df3967a0e590c68e13fb4773ae
macheng2017 <gos360@163.com>
2020-02-27 15:32:50 +0800

    feat:在使用自动类型推断的时候可以将不同类型的变量放到一行上进行赋值

--- basic/basic.go
+++ basic/basic.go
@@ -10,8 +10,24 @@ func variableZeroValue() {
 
 // hello world
 // 0,'\x00' 使用%q打印出空串
+
+func variableInitialValue() {
+	var a, b int = 3, 4
+	var s string = "abc"
+	fmt.Println(a, b, s)
+} // 3 4 abc
+
+func variableTypeDeduction() {
+	// 在使用自动类型推断的时候可以将不同类型的变量放到一行上进行赋值
+	var a, b, c, d = 3, 4, true, "def"
+	fmt.Println(a, b, c, d)
+
+} // go 语言中的类型自动推断(type deduction)
+// 3 4 true def
+
 func main() {
 	fmt.Print("hello world\n")
-
 	variableZeroValue()
+	variableInitialValue()
+	variableTypeDeduction()
 }
